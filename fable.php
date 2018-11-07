d2b0cc4a11c3abe86ce612b8cb0571a916059128
MonSeigneurDEV <tinou.27@hotmail.fr>
2018-11-07 15:46:57 +0100

    'trankil'

---
+++ fable.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>fable</title>
+  </head>
+  <body>
+    <p class="verset1">Maître Corbeau, sur un arbre perché,
+Tenait en son bec un fromage.
+Maître Renard, par l'odeur alléché,
+Lui tint à peu près ce langage :
+Et bonjour, Monsieur du Corbeau.</p>
+  </body>
+</html>
